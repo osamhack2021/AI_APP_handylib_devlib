a7d07f4dd128ed891970ad3845c5f8d3f419b669
Jordano-Jackson <eat333@naver.com>
2021-09-15 14:39:31 +0000

    login 및 route 파일 설정

---
+++ app/lib/components/book_list_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BookListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+  final String explanation;
+
+  const BookListItem(this.imageName, this.title, this.explanation);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
