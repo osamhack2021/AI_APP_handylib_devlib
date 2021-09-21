9dbfa76b6b73f27c3bb60d7a7d7a47b8c12e4f0d
1kl1 <mamy0320@naver.com>
2021-09-22 08:37:15 +0900

    first init

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,18 @@
-
-import 'package:app/pages/book_list_page.dart';
-import 'package:app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:app/routes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: "\login",
-      routes: {
-        "\login" :(context) => LoginPage(),
-        //"\home" : (context) => HomePage()
-      }
+      routes: routes,
     );
   }
 }
---
+++ app/lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:app/pages/login_page.dart';
+
+final routes = {
+  "/login": (BuildContext context) => LoginPage(),
+};
