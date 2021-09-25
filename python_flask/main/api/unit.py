f3409164074ae66ca979d81a747d2636ee576ad4
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-09-25 14:26:44 +0000

    unit_add

---
+++ python_flask/main/api/unit.py
@@ -0,0 +1,29 @@
+#Unit blueprint
+from flask import Blueprint,request,Flask
+from flask.templating import render_template
+from flask_mongoengine import MongoEngine
+from mongoengine.errors import ValidationError
+from mongoengine.fields import StringField
+from main.models import database
+def not_empty(val):
+    if not val:
+        return ValidationError('error')
+
+Unit=Blueprint('unit',__name__,url_prefix='/unit')
+
+@Unit.route('/',methods=['GET','POST'])
+def unit_name():
+    if request.method=='GET':
+        u = []
+        for i in range(0,len(database.Unit.objects())):
+            u.append(database.Unit.objects[i].name)
+        return ", ".join(u)
+    else:
+        return "Unit_name 클릭하면 url 및 값 넘기기"
+
+@Unit.route('/<Unit_name>', methods=['POST'])
+def Unit_books_list(Unit_name):
+  u = ""
+#   u = ", ".join(database.Unit.objects(name=Unit_name)[0].books_list) 왜 파라메터 값으로 받아서 파싱하려는데 에러뜨지;;;; Colab에서 잘되는데ㅠㅠ
+  u = ", ".join(database.Unit.objects(name="53사단")[0].books_list)
+  return u
