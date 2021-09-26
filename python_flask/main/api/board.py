657a7fff5b3c7e8d20271a52457ec6ed96571bd6
Eeap <42088290+Eeap@users.noreply.github.com>
2021-09-26 11:34:26 +0000

    comment 작성 및 path 수정

--- python_flask/main/api/board.py
+++ python_flask/main/api/board.py
@@ -1,6 +1,5 @@
-from werkzeug.utils import redirect
 from main.api import login_require
-from flask import Blueprint, json,request,Response,session
+from flask import Blueprint, json,request,Response,session,redirect
 from main.models import database
 from datetime import datetime
 import time
@@ -22,10 +21,10 @@ def boarding(page_id):
     return Response(resultJson,mimetype="application/json",status=200)
     #pageid에 따라 게시판 수량 띄우기
 #해당 페이지 불러오는 라우터
-@board_page.route('/<int:number>',methods=['GET'])
+@board_page.route('/page/<int:number>',methods=['GET'])
 @login_require
 def board_number(number):
-    board_item=database.Notice_board.objects(number=number,tag=request.args.get('tag')).to_json()
+    board_item=database.Notice_board.objects(number=number,tag=request.args.get('tag')).first().to_json()
     resultJson=json.dumps(board_item)
     return Response(resultJson,mimetype="application/json",status=200)
 #작성 요청 라우터
@@ -39,21 +38,21 @@ def write_board():
     database.Notice_board(content=content,title=title
     ,user_id=user_id,tag=tag,
     time_stamp=datetime.fromtimestamp(time.time()),
-    number=database.Notice_board.objects(tag=request.form.get('tag')).count()+1).save()
-    return redirect('/board/1')
+    number=database.Notice_board.objects(tag=tag).count()+1).save()
+    return redirect('/board/page/{number}')
 
 #수정 요청 라우터
-@board_page.route('/<int:number>/edit',methods=['PUT'])
+@board_page.route('/page/<int:number>/edit',methods=['PUT'])
 @login_require
 def edit_board(number):  
     board_item = database.Notice_board.objects(number=number,tag=request.form.get('tag')).first()
     board_item.update(title=request.form.get('title'),
     content=request.form.get('content')
     )
-    return redirect('/board/{number}')
+    return redirect('/board/page/{number}')
 
 #삭제 요청 라우터
-@board_page.route('/<int:number>/delete',methods=['DELETE'])
+@board_page.route('/page/<int:number>/delete',methods=['DELETE'])
 @login_require
 def delete_board(number):
     board_item = database.Notice_board.objects(number=number,tag=request.form.get('tag')).first()
