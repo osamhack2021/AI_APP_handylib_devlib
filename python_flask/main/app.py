9bdf7c3749a715a61fa0254deb466fccfe69d80a
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-09-28 14:46:25 +0000

    searhchlog_add

--- python_flask/main/app.py
+++ python_flask/main/app.py
@@ -1,6 +1,6 @@
 #시작 페이지
 from flask import Flask, redirect,render_template
-from main.api import sign_up,sign_in,logout,board,unit,comment,account_info
+from main.api import sign_up,sign_in,logout,board,unit,comment,account_info,searchlog
 app = Flask(__name__)
 app.register_blueprint(sign_up.sign_up_page,url_prefix='/sign_up')
 app.register_blueprint(sign_in.sign_in_page,url_prefix='/sign_in')
@@ -9,6 +9,7 @@ app.register_blueprint(board.board_page,url_prefix='/board')
 app.register_blueprint(unit.unit_page,url_prefix='/unit')
 app.register_blueprint(comment.comment_page,url_prefix='/board/page')
 app.register_blueprint(account_info.info_page,url_prefix='/info')
+app.register_blueprint(searchlog.info_page,url_prefix='/searclog')
 app.debug = True
 
 
