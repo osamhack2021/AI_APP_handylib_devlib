63794aa86ba039a7e77e0139b8f09e8847fd1929
Eeap <42088290+Eeap@users.noreply.github.com>
2021-09-29 11:16:22 +0000

    로그인, 로그아웃, 개인정보 페이지 res 수정

--- python_flask/main/api/sign_in.py
+++ python_flask/main/api/sign_in.py
@@ -1,25 +1,25 @@
-from flask import Blueprint,request,session,redirect,render_template
+from flask import Blueprint,request,session,json,Response
 from main.models import database
 sign_in_page=Blueprint('sign_in',__name__)
 
-@sign_in_page.route('/',methods=['GET','POST'])
+@sign_in_page.route('/',methods=['POST'])
 def sign_in():
-    if request.method=='GET':
-        return render_template('sign_in.html')
+
+    user_id = request.form.get('user_id')
+    pwd = request.form.get('password')
+    users = database.User.objects(user_id=user_id).first()
+    if not users:
+        #response형태로 id 잘못 입력 보내기
+        resultJson=json.dumps({"message": "login failed"})
+        return Response(resultJson,mimetype="application/json",status=401)
     else:
-        user_id = request.form.get('user_id')
-        pwd=request.form.get('password')
-        users=database.User.objects(user_id=user_id).first()
-        if not users:
-            #response형태로 id 잘못 입력 보내기
-            return redirect('/sign_in')
-        else:
-            if users.password==pwd:
-                session["user_id"]=user_id
-                session["name"]=users.name
-                session.permanent=True
-                return redirect('/')
-            #실패시 response형태로 pwd 잘못 입력 보내기
-            return redirect('/sign_in')
-        
-        
+        if users.password == pwd:
+            session["user_id"] = user_id
+            session["name"] = users.name
+            session.permanent = True
+            #성공시 response 200 status
+            resultJson=json.dumps({"message": "login success"})
+            return Response(resultJson,mimetype="application/json",status=200)
+        #실패시 response형태로 pwd 잘못 입력 보내기
+        resultJson=json.dumps({"message": "login failed"})
+        return Response(resultJson,mimetype="application/json",status=401)
--- python_flask/main/api/sign_up.py
+++ python_flask/main/api/sign_up.py
@@ -1,29 +1,26 @@
 #sign_up blueprint
-from flask import Blueprint, request,redirect,render_template
+from flask import Blueprint, request, json, Response
 from main.models import database
 
 sign_up_page = Blueprint('sign_up', __name__)
 
 
-@sign_up_page.route('/', methods=['GET', 'POST'])
+@sign_up_page.route('/', methods=['POST'])
 def sign_up():
-    if request.method == 'GET':
-        #회원가입페이지 렌더링
-        return render_template('sign_up.html')
-    else:
-        #user_id가 일치하는게 db에 있는지 확인
-        user = database.User.objects(user_id=request.form.get('user_id')).first()
-        if user:
-            return redirect('/sign_up')
-        database.User(
-            user_id=request.form.get('user_id'),
-            password=request.form.get('password'),
-            name=request.form.get('name'),
-            email=request.form.get('email'),
-            interest_tag=list(request.form.get('interest_tag')),
-            rank=request.form.get('rank'),
-            unit=request.form.get('unit')
-        ).save()
-        
-    #db저장 작업이 끝나면 다른 페이지로 렌더링
-    return redirect('/sign_in')
+    #user_id가 일치하는게 db에 있는지 확인
+    user = database.User.objects(
+        user_id=request.form.get('user_id')).first()
+    if user:
+        resultJson=json.dumps({"message": "user already exists"})
+        return Response(resultJson,mimetype="application/json",status=403)
+    database.User(
+        user_id=request.form.get('user_id'),
+        password=request.form.get('password'),
+        name=request.form.get('name'),
+        email=request.form.get('email'),
+        interest_tag=list(request.form.get('interest_tag')),
+        rank=request.form.get('rank'),
+        unit=request.form.get('unit')
+    ).save()
+    resultJson=json.dumps({"message": "sign_up success"})
+    return Response(resultJson,mimetype="application/json",status=200)
