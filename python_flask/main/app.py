123a0f8b1418b621d331cafc8630bbbb5c0d3933
Eeap <42088290+Eeap@users.noreply.github.com>
2021-09-29 03:09:26 +0000

    app 수정

--- python_flask/main/app.py
+++ python_flask/main/app.py
@@ -1,6 +1,5 @@
 #시작 페이지
 from flask import Flask, redirect,render_template
-from main.api import sign_up,sign_in,logout,board,unit,comment,account_info,mypage
 from main.api import sign_up,sign_in,logout,board,unit,comment,account_info,searchlog
 app = Flask(__name__)
 app.register_blueprint(sign_up.sign_up_page,url_prefix='/sign_up')
