d421df76cb036928c473f768b351b474bd1f7861
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-04 08:08:31 +0000

    book_fix1

--- python_flask/main/api/book.py
+++ python_flask/main/api/book.py
@@ -8,6 +8,10 @@ unit_page=Blueprint('book',__name__)
 
 @book_page.route('/search=<title>',methods=['GET','POST'])
 def book_search():
-    a = list (database.client.DevLib.book.find( {'title': { '$regex': '{0}'.format(title), '$options': 'i' }} ))
-    return a[0]['title'] 
+    ajson = []
+    a = list (client.DevLib.book.find( {'title': { '$regex': '{}'.format(title), '$options': 'i' }} ))
+    for o in range(len(a)):
+        del(a[o]['_id'])
+        ajson.append(json.dumps(a[o]))
+    return ajson[0]
     #예외코드 안만듬, "[세트] 달러구트 꿈 백화점 1~2 - 전2권" 만 검색가능
--- python_flask/main/api/unit.py
+++ python_flask/main/api/unit.py
@@ -30,3 +30,20 @@ def Unit_books_list(Unit_name):
             return "부대가 없거나 올바른 부대를 입력해주세요"
         else :
             return "동일한 부대 데이터가 2개이상 존재합니다."
+
+@unit_page.route('/<Unit_name>/best', methods=['GET'])
+def Unit_books_best(Unit_name):
+    c = 0
+    for i in range(0,len(Unit.objects())):
+        if Unit.objects[i].name == '{0}'.format(Unit_name:
+            c += 1
+    if c == 1 :
+        # u = ""
+        # u = ", ".join(Unit.objects(name='{0}'.format(Unit_name))[0].books_list)
+        return Unit.objects(name='{0}'.format(Unit_name))[0].books_list[0:9]
+        #return Unit.objects(name='{0}'.format(Unit_name))[0].to_json()
+    else :
+        if c == 0 :
+            return "부대가 없거나 올바른 부대를 입력해주세요"
+        else :
+            return "동일한 부대 데이터가 2개이상 존재합니다."
