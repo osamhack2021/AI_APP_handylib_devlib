1c18782e90dac8363ae231cf8cb36530461d45a7
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-06 13:20:23 +0000

    searchlog_10개 추가

--- python_flask/main/api/searchlog.py
+++ python_flask/main/api/searchlog.py
@@ -1,33 +1,47 @@
 #search blueprint
 from flask import Blueprint
 from main.models import database
+import json
 
 search_page=Blueprint('search',__name__)
 
 @search_page.route('/<user_id>/search=<keyword>',methods=['POST'])
 def search_keyword(user_id, keyword):
-    if len(database.Searchlog.objects(user_id='{0}'.format(user_id))) == 0 :
+    sob = database.Searchlog.objects(user_id='{0}'.format(user_id))
+    if len(sob) == 0 :
         database.Searchlog(user_id='{0}'.format(user_id)).save()
     u = []
-    u = "// ".join(database.Searchlog.objects(user_id='{0}'.format(user_id))[0].log)
+    u = "// ".join(sob[0].log)
     if u == '' :
         u += '{0}'.format(keyword)
     else :
         u += '// {0}'.format(keyword)
-    database.Searchlog.objects(user_id='{0}'.format(user_id))[0].update(log = u.split('// '))
-    return "search_success" # 검색 목록 띄우기
+    sob[0].update(log = u.split('// '))
+    return sob[0].to_json()
 
 @search_page.route('/<user_id>/searchlog',methods=['GET'])
 def search_log(user_id):
-    if len(database.Searchlog.objects(user_id='{0}'.format(user_id))) == 0 :
+    sob = database.Searchlog.objects(user_id='{0}'.format(user_id))
+    if len(sob) == 0 :
         database.Searchlog(user_id='{0}'.format(user_id)).save()
-    u = ""
-    u = ", ".join(database.Searchlog.objects(user_id='{0}'.format(user_id))[0].log)
-    return u
+    return sob[0].to_json()#.get('log')
+
+@search_page.route('/<user_id>/searchlog=10',methods=['POST'])
+def search_log_10(user_id):
+    sob = database.Searchlog.objects(user_id='{0}'.format(user_id))
+    if len(sob) == 0 :
+        sob.save()
+    if len(sob[0]['log']) <= 10:
+        return sob[0].to_json()
+    else :
+        jsl = {'user_Id': '{0}'.format(user_id) , 'log': sob[0]['log'][len(sob[0]['log'])-10:len(sob[0]['log'])]}
+        jsl = json.dumps(jsl)
+    return jsl
 
 @search_page.route('/<user_id>/searchlog=delete',methods=['POST'])
 def search_log_delete(user_id):
-    if len(database.Searchlog.objects(user_id='{0}'.format(user_id))) == 0 :
+    sob = database.Searchlog.objects(user_id='{0}'.format(user_id))
+    if len(sob) == 0 :
         database.Searchlog(user_id='{0}'.format(user_id)).save()
-    database.Searchlog.objects(user_id='{0}'.format(user_id))[0].update(log = [])
-    return "delete_success" # 삭제성공 메시지 or 사라진 검색log 출력
+    sob[0].update(log = [])
+    return sob[0].to_json()#.get('log')
