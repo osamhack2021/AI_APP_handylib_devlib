37274dba4c3e7dff13397334eade83e4cd8d1641
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-06 15:00:18 +0000

    unit fix1

--- python_flask/main/api/unit.py
+++ python_flask/main/api/unit.py
@@ -1,49 +1,53 @@
 #Unit blueprint
-from flask import Blueprint, request
+from flask import Blueprint, json, Response
 from main.models import database
 
 unit_page=Blueprint('unit',__name__)
 
 @unit_page.route('/',methods=['GET','POST'])
 def unit_name():
-    if request.method=='GET':
-        u = []
-        for i in range(0,len(database.Unit.objects())):
-            u.append(database.Unit.objects[i].name)
-        return ", ".join(u)
-    else:
-        return "Unit_name 클릭하면 url 및 값 넘기기"
+    u = []
+    for i in range(0,len(database.Unit.objects())):
+        u.append(database.Unit.objects[i].name)
+    jsl = {'Unit_names': u }
+    resultJson = json.dumps(jsl)
+    return Response(resultJson,mimetype="application/json",status=200)
 
 @unit_page.route('/<Unit_name>', methods=['POST'])
 def Unit_books_list(Unit_name):
     c = 0
-    for i in range(0,len(Unit.objects())):
-        if Unit.objects[i].name == '{0}'.format(Unit_name):
+    for i in range(0,len(database.Unit.objects())):
+        if database.Unit.objects[i].name == '{0}'.format(Unit_name):
             c += 1
     if c == 1 :
-        u = ""
-        u = ", ".join(Unit.objects(name='{0}'.format(Unit_name))[0].books_list)
-        return u
-        #return Unit.objects(name='{0}'.format(Unit_name))[0].to_json()
+        resultJson = database.Unit.objects(name='{0}'.format(Unit_name))[0].to_json()
+        return Response(resultJson,mimetype="application/json",status=200)
     else :
         if c == 0 :
-            return "부대가 없거나 올바른 부대를 입력해주세요"
+            jsl = {'Error': '부대가 없거나 올바른 부대를 입력해주세요.'}
+            resultJson = json.dumps(jsl)
+            return Response(resultJson,mimetype="application/json",status=201)
         else :
-            return "동일한 부대 데이터가 2개이상 존재합니다."
+            jsl = {'Error': '동일한 부대 데이터가 2개이상 존재합니다.'}
+            resultJson = json.dumps(jsl)
+            return Response(resultJson,mimetype="application/json",status=201)
 
-@unit_page.route('/<Unit_name>/best', methods=['GET'])
+@unit_page.route('/<Unit_name>/best', methods=['GET', 'POST'])
 def Unit_books_best(Unit_name):
     c = 0
-    for i in range(0,len(Unit.objects())):
-        if Unit.objects[i].name == '{0}'.format(Unit_name):
+    for i in range(0,len(database.Unit.objects())):
+        if database.Unit.objects[i].name == '{0}'.format(Unit_name):
             c += 1
     if c == 1 :
-        # u = ""
-        # u = ", ".join(Unit.objects(name='{0}'.format(Unit_name))[0].books_list)
-        return Unit.objects(name='{0}'.format(Unit_name))[0].books_list[0:9]
-        #return Unit.objects(name='{0}'.format(Unit_name))[0].to_json()
+        jsl = {'name': '{0}'.format(Unit_name), 'books_list': database.Unit.objects(name='{0}'.format(Unit_name))[0].books_list[0:9] }
+        resultJson = json.dumps(jsl)
+        return Response(resultJson,mimetype="application/json",status=200)
     else :
         if c == 0 :
-            return "부대가 없거나 올바른 부대를 입력해주세요"
+            jsl = {'Error': '부대가 없거나 올바른 부대를 입력해주세요.'}
+            resultJson = json.dumps(jsl)
+            return Response(resultJson,mimetype="application/json",status=201)
         else :
-            return "동일한 부대 데이터가 2개이상 존재합니다."
+            jsl = {'Error': '동일한 부대 데이터가 2개이상 존재합니다.'}
+            resultJson = json.dumps(jsl)
+            return Response(resultJson,mimetype="application/json",status=201)
