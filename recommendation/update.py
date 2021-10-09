79bd4e810e139212971156cd44cdd902b0bca11a
k2river <kstream35@gmail.com>
2021-10-09 16:24:42 +0900

    update.py updater parameter 에 파일명 추가

--- recommendation/update.py
+++ recommendation/update.py
@@ -3,10 +3,10 @@ from recommendation import recommendation
 import pandas as pd
 # import numpy as np
 
-def updater(file_path):
+def updater(file_path, users_file_name, books_file_name):
     # file_path = 'C:/Users/admin/Documents/osam_ai/book_dataset/'  # 서버 폴더경로 맞춰서 다시 설정
-    users_file_name = "rec_user_1.csv"
-    books_file_name = "rec_books_1.csv"
+    # users_file_name = "rec_user_1.csv"
+    # books_file_name = "rec_books_1.csv"
     pred_file_name = "rec_pred_score_1.csv"
     df_users = pd.read_csv(file_path + users_file_name, encoding='cp949')
 
