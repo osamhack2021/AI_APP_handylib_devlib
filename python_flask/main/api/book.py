21b872f1c854eb0068e8e17d0ddf5748531df61a
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-11 08:14:15 +0000

    book fix1

--- python_flask/main/api/book.py
+++ python_flask/main/api/book.py
@@ -7,7 +7,7 @@ book_page=Blueprint('book',__name__)
 @book_page.route('/search/title=<title>',methods=['GET','POST'])
 def book_search():
     ajson = []
-    a = list (database.client.DevLib.book.find( {'title': { '$regex': '{}'.format(title), '$options': 'i' }} ))
+    a = list (database.client.API_test.book.find( {'title': { '$regex': '{}'.format(title) }} ))
     for o in range(len(a)):
         del(a[o]['_id'])
         ajson.append(json.dumps(a[o]))
@@ -23,8 +23,8 @@ def book_search_categoryId(categoryId, page):
     if page <= 1:
         page = 0
     else:
-        page = page-1
-    a = list (database.client.DevLib.book.find( {'categoryId': categoryId} ).skip(page*5).limit(5))
+        page = page - 1
+    a = list (database.client.API_test.book.find( {'categoryId': categoryId} ).skip(page*5).limit(5))
     for o in range(len(a)):
         del(a[o]['_id'])
         ajson.append((a[o]))
@@ -40,15 +40,11 @@ def book_search_feed_categoryId(categoryId, page):
     if page <= 1:
         page = 0
     else:
-        page = page-1
-    a = list (database.client.DevLib.book.find( {'categoryId': categoryId} ).skip(page*5).limit(5))
+        page = page - 1
+    a = list (database.client.API_test.book.find( {'categoryId': categoryId} ).skip(page*5).limit(5))
     for o in range(len(a)):
         del(a[o]['_id'])
         ajson.append((a[o]))
     result = {'list': ajson }
     resultJson = json.dumps(result, ensure_ascii=False)
     return Response(resultJson,mimetype="application/json",status=200)
-
-@book_page.route('/feed/best/page=<page>',methods=['GET','POST'])
-def book_search_feed_best(page):
-    return "NULL"
