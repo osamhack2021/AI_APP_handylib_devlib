2c554cca0d5bcf5d312b6d82fd3b1cb49356c35e
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-11 08:24:07 +0000

    unit 대여반납기능코드 및 수정 부분 확인

--- python_flask/main/api/unit.py
+++ python_flask/main/api/unit.py
@@ -20,7 +20,7 @@ def Unit_books_list(Unit_name):
         if database.Unit.objects[i].name == '{0}'.format(Unit_name):
             c += 1
     if c == 1 :
-        resultJson = database.Unit.objects(name='{0}'.format(Unit_name))[0].to_json()
+        # resultJson = database.Unit.objects(name='{0}'.format(Unit_name))[0].to_json() *embook에 맞게 리턴수정 예정
         return Response(resultJson,mimetype="application/json",status=200)
     else :
         if c == 0 :
@@ -39,7 +39,7 @@ def Unit_books_best(Unit_name):
         if database.Unit.objects[i].name == '{0}'.format(Unit_name):
             c += 1
     if c == 1 :
-        jsl = {'name': '{0}'.format(Unit_name), 'books_list': database.Unit.objects(name='{0}'.format(Unit_name))[0].books_list[0:9] }
+        # jsl = {'name': '{0}'.format(Unit_name), 'books_list': database.Unit.objects(name='{0}'.format(Unit_name))[0].books_list[0:9] } *score기반으로 best선정예정
         resultJson = json.dumps(jsl, ensure_ascii=False)
         return Response(resultJson,mimetype="application/json",status=200)
     else :
@@ -51,3 +51,24 @@ def Unit_books_best(Unit_name):
             jsl = {'Error': '동일한 부대 데이터가 2개이상 존재합니다.'}
             resultJson = json.dumps(jsl, ensure_ascii=False)
             return Response(resultJson,mimetype="application/json",status=201)
+
+#확인하는코드
+for i in Unit.objects(name='test1')[0]['books_list']:
+  if i['isbn'] == '8937479958':
+    print(Embook(title=i['title'],isbn=i['isbn'],isbn13=i['isbn13'],state=i['state'],user_id=i['user_id'],score=i['score']).to_json())
+
+# 대여하는 코드
+for i in Unit.objects(name='test1')[0]['books_list']:
+  if i['isbn'] == '8937479958':
+    c = {'title': i['title'],'isbn': i['isbn'],'isbn13': i['isbn13'],'state': 1,'user_id': i['user_id'],'score': i['score'] + 1,'_cls': 'Embook'}
+client.API_test.unit.update({'name':'test1'}, {'$pull': {'books_list': {'isbn':'8937479958'}}})    
+client.API_test.unit.update({'name':'test1'}, {'$push': {'books_list': c}})
+# user_id도 기입하게 수정해야됨
+
+# 반납하는 코드
+for i in Unit.objects(name='test1')[0]['books_list']:
+  if i['isbn'] == '8937479958':
+    c = {'title': i['title'],'isbn': i['isbn'],'isbn13': i['isbn13'],'state': 0,'user_id': '','score': i['score'],'_cls': 'Embook'}
+client.API_test.unit.update({'name':'test1'}, {'$pull': {'books_list': {'isbn':'8937479958'}}})
+client.API_test.unit.update({'name':'test1'}, {'$push': {'books_list': c}})
+# 반납하는 유저가 맞는지 확인하는 방어코드 필요
