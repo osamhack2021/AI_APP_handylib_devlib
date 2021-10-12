8466e68e6144976e0328c312db7a825440ada6ff
B-1Go <86540113+B-1Go@users.noreply.github.com>
2021-10-12 14:37:47 +0000

    book 블루프린트추가

--- python_flask/main/app.py
+++ python_flask/main/app.py
@@ -1,6 +1,6 @@
 #blueprint 관리
 from flask import Flask
-from main.api import sign_up,sign_in,logout,board,unit,comment,account_info,searchlog,mypage
+from main.api import sign_up,sign_in,logout,board,unit,comment,account_info,searchlog,mypage,book
 app = Flask(__name__)
 app.register_blueprint(sign_up.sign_up_page,url_prefix='/sign_up')
 app.register_blueprint(sign_in.sign_in_page,url_prefix='/sign_in')
@@ -11,4 +11,5 @@ app.register_blueprint(comment.comment_page,url_prefix='/board/page')
 app.register_blueprint(account_info.info_page,url_prefix='/info')
 app.register_blueprint(mypage.mypage_page,url_prefix='/mypage')
 app.register_blueprint(searchlog.search_page,url_prefix='/searchlog')
+app.register_blueprint(book.book_page,url_prefix='/book')
 app.debug = False
