e3584bc529b43463160933410ed3da47be156c90
k2river <kstream35@gmail.com>
2021-10-18 22:18:59 +0900

    서버 연동 후 코드 수정

--- AI/update.py
+++ AI/update.py
@@ -1,15 +1,8 @@
-from ALS import predict
-from apscheduler.schedulers.background import BackgroundScheduler
+from .ALS import predict
 
-def job():
-    file_path = '/var/www/python_flask/main/models/'  # 서버 폴더경로 맞춰서 설정
-    users_file_name = "API_test_users.csv"
-    updater(file_path, users_file_name)
-
-def updater(file_path, users_file_name):
+def update():
+    file_path = '/var/www/python_flask/main/recommendation/'
+    users_file_name = 'API_test_users.csv'
+    books_file_name = 'API_test_books.csv'
     pred_file_name = "rec_pred_score_1.csv"
-    predict(file_path, users_file_name, pred_file_name)
-
-sched = BackgroundScheduler()
-sched.start()
-sched.add_job(job, 'cron', hour='*/1', id="update pred_score")
+    predict(file_path, users_file_name, books_file_name, pred_file_name)
--- AI/wordcloud_maker.py
+++ AI/wordcloud_maker.py
@@ -3,16 +3,39 @@ from wordcloud import STOPWORDS
 
 import pandas as pd
 import numpy as np
-import os, shutil
 
 def make_wordcloud(file_path, save_path, font_path, users_file_name, books_file_name, user_id):
     stopwords = set(STOPWORDS)
     stopwords.add("국내도서")
-    df_users = pd.read_csv(file_path + users_file_name, encoding='cp949')
-    df_users_like_borrowed_interesttags = pd.DataFrame(df_users, columns=['user_id', 'like', 'borrowed', 'interest_tag'])
-    df_users_like_borrowed_interesttags = df_users_like_borrowed_interesttags[df_users_like_borrowed_interesttags['user_id']==user_id]
+    df_users = pd.read_csv(file_path + users_file_name, encoding='UTF8')
+    df_users_like = pd.DataFrame(df_users, columns=['user_id', 'like'])
+    df_users_like = df_users_like[df_users_like['user_id']==user_id]
 
-    str_like = list(np.array(df_users_like_borrowed_interesttags['like'].tolist()))
+    val = df_users_like['like'].head(1)
+    val = list(np.array(val))
+    if df_users_like.empty or val == ['[]']:
+        df_books = pd.read_csv(file_path + books_file_name, encoding='UTF8')
+        df_books_tag = pd.DataFrame(df_books, columns = ['categoryName'])
+
+        list_books_tag = np.array(df_books_tag.iloc[:, 0].tolist())
+        list_books_tag = list(map(lambda x: x.split('>'), list_books_tag))
+        list_books_tag = sum(list_books_tag)
+        str_books_tag = " ".join(list_books_tag)
+
+        wc = WordCloud(font_path=font_path,
+                       background_color='white',
+                       width=400,
+                       height=300,
+                       max_words=200,
+                       max_font_size=50,
+                       stopwords=stopwords)
+
+        wc. generate(str_books_tag)
+        file_name = str(user_id) + '.png'
+        wc.to_file(save_path + file_name)
+        return
+
+    str_like = list(np.array(df_users_like['like'].tolist()))
     i = str_like[0].lstrip('[').rstrip(']')
     i = i.split(', ')
     i = list(map(int, i))
@@ -27,7 +50,7 @@ def make_wordcloud(file_path, save_path, font_path, users_file_name, books_file_
     list_books_tag = sum(list_books_tag, [])
     str_books_tag = " ".join(list_books_tag)
 
-    wc = WordCloud(font_path=font_path, #"C:/Users/admin/Documents/osam_ai/dev_rec/NanumFontSetup_TTF_BARUNPEN/NanumBarunpenR.ttf",
+    wc = WordCloud(font_path=font_path,
             background_color="white",
             width=400,
             height=300,
@@ -36,17 +59,13 @@ def make_wordcloud(file_path, save_path, font_path, users_file_name, books_file_
             stopwords=stopwords)
 
     wc.generate(str_books_tag)
-    file_name = 'favoriteCategory_'+str(user_id)+'.png'
-    wc.to_file(file_name)
+    file_name = str(user_id)+'.png'
+    wc.to_file(save_path + file_name)
 
-    path = os.getcwd()
-    shutil.move(path+"\\"+file_name, save_path+"\\"+file_name)
-
-
-file_path = 'C:/Users/admin/Documents/osam_ai/book_dataset/'
-save_path = file_path
-users_file_name = "API_test_users.csv"
-books_file_name = "API_test_books.csv"
-font_path = "C:/Users/admin/Documents/osam_ai/dev_rec/NanumFontSetup_TTF_BARUNPEN/NanumBarunpenR.ttf"
-user_id = 0
-make_wordcloud(file_path, save_path, font_path, users_file_name, books_file_name, user_id)
+def exe_img(user_id):
+    file_path = '/var/www/python_flask/main/recommendation/'
+    save_path = '/var/www/python_flask/main/static/img/'
+    users_file_name = "API_test_users.csv"
+    books_file_name = "API_test_books.csv"
+    font_path = "/var/www/python_flask/main/recommendation/NanumFontSetup_TTF_BARUNPEN/NanumBarunpenR.ttf"
+    make_wordcloud(file_path, save_path, font_path, users_file_name, books_file_name, user_id)
